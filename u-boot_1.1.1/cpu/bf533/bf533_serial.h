f7c8a5322ab5143b8f00535ac1cd272d37a7083a
LG Soft India <satheesh_s@lgsoftindia.com>
2004-11-30 14:03:51 +0000

    clock related changed are incorporated
    git-svn-id: svn://localhost/svn/u-boot/trunk@151 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.1/cpu/bf533/bf533_serial.h
+++ u-boot_1.1.1/cpu/bf533/bf533_serial.h
@@ -37,7 +37,6 @@ void serial_puts(const char *s);
 static void local_put_char(char ch);
 
 extern int get_clock(void);
-extern unsigned long sclk;
 int baud_table[5] = {9600, 19200, 38400, 57600, 115200};
 
 struct {
