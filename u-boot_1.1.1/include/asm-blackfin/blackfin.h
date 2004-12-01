1620a33562c85b275012cd7fc4a178bc9fd56193
LG Soft India <satheesh_s@lgsoftindia.com>
2004-12-01 20:22:16 +0000

    VDSP header files included
    git-svn-id: svn://localhost/svn/u-boot/trunk@174 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.1/include/asm-blackfin/blackfin.h
+++ u-boot_1.1.1/include/asm-blackfin/blackfin.h
@@ -1,4 +1,22 @@
 #ifndef _BLACKFIN_H_
 #define _BLACKFIN_H_
-#include <asm/board.h>
+
+#include <asm/cpu/defBF533.h>
+#include <asm/cpu/bf533_serial.h>
+
+#ifndef __ASSEMBLY__
+#ifndef ASSEMBLY
+
+#ifdef SHARED_RESOURCES
+ #include <asm/shared_resources.h>
+#endif
+#include <asm/cpu/cdefBF53x.h>
+
+#endif
+#endif
+
+#include <asm/cpu/defBF533.h>
+#include <asm/cpu/defBF533_extn.h>
+#include <asm/cpu/bf533_serial.h>
+
 #endif
