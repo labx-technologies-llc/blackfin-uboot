ccfd00cf5918af8995bbd6a14d54832928e2f369
LG Soft India <satheesh_s@lgsoftindia.com>
2004-12-01 20:14:44 +0000

    OBJCFLAGS added
    git-svn-id: svn://localhost/svn/u-boot/trunk@161 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.1/board/stamp/config.mk
+++ u-boot_1.1.1/board/stamp/config.mk
@@ -23,3 +23,11 @@
 
 TEXT_BASE = 0x20000000
 PLATFORM_CPPFLAGS += -I$(TOPDIR)
+
+# OBJCFLAGS options to be given:
+# --set-start=0x00 --adjust-section-vma=.text-$(TEXT_BASE)
+#                  --adjust-section-vma=.text1-($(CFG_MONITOR_BASE) - XIP code size)
+#                  --adjust-section-vma=.data-($(CFG_MONITOR_BASE) - XIP code size)
+# where XIP code size is - size of instructions that got compiled to run from flash = .text code size
+#                  This is same as the .text size outputed in u-boot.map
+OBJCFLAGS = --set-start=0x00 --adjust-section-vma=.text-0x20000000 --adjust-section-vma=.text1-0x7f7fd7c --adjust-section-vma=.data-0x7f7fd7c
