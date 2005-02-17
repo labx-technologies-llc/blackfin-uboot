fe14c49298562c4ce038c88ce988e1686ff4b7cc
LG Soft India <satheesh_s@lgsoftindia.com>
2005-02-17 11:10:39 +0000

    uboot software protection code added
    git-svn-id: svn://localhost/svn/u-boot/trunk@186 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.1/board/stamp/flash.c
+++ u-boot_1.1.1/board/stamp/flash.c
@@ -1,5 +1,30 @@
 /*
- * (C) Copyright 2004 -  Analog Devices, Inc.  All rights reserved. 
+ * U-boot - flash.c Flash driver for MW320DB
+ *
+ * Copyright (c) 2005 blackfin.uclinux.org
+ *
+ * This file is based on STAMP_Drivers.c originally written by Analog Devices, Inc.
+ *
+ * (C) Copyright 2000-2004
+ * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
  */
 
 #include "flash-defines.h"
@@ -16,23 +41,23 @@ unsigned long flash_get_size(ulong baseaddr, flash_info_t * info)
 	man_id = GetCodes();
 	dev_id = man_id & 0xffff;
 	man_id = (man_id >> 16) & 0xff;
-
+#ifdef DEBUG
 	printf("Device ID of the Flash is %x\n", dev_id);
 	printf("Manufacture ID of the Flash is %x\n", man_id);
 	printf("Memory Map of Flash: 0x%x - 0x%x\n",CFG_FLASH0_BASE,(CFG_FLASH0_BASE+FLASH_SIZE)-1);
-
+#endif
 	info->flash_id = dev_id;
 	info->sector_count = FLASH_TOT_SECT;
 	info->size = FLASH_SIZE;
 
-	info->start[0] = SECT1_ADDR; 
+	info->start[0] = SECT1_ADDR;
 	info->start[1] = SECT2_ADDR;
 	info->start[2] = SECT3_ADDR;
 	info->start[3] = SECT4_ADDR;
 
 	for (i = 4; i < 67; i++)
 		info->start[i] = ((baseaddr + AFP_SectorSize1)
-				  + ((i - 4) * AFP_SectorSize1));
+				+ ((i - 4) * AFP_SectorSize1));
 
 	return (info->size);
 }
@@ -42,18 +67,22 @@ unsigned long flash_init(void)
 	unsigned long size_b0 = 0;
 	int i;
 
+#ifdef DEBUG
 	printf("Flash Memory Start 0x%x\n", CFG_FLASH_BASE);
 	printf("Please type command flinfo for information on Sectors \n");
+#endif
 	swap_to(FLASH);
 	for (i = 0; i < CFG_MAX_FLASH_BANKS; ++i) {
 		flash_info[i].flash_id = FLASH_UNKNOWN;
 	}
 	size_b0 = flash_get_size(CFG_FLASH0_BASE, &flash_info[0]);
 	if (flash_info[0].flash_id == FLASH_UNKNOWN || size_b0 == 0) {
-		printf
-		    ("## Unknown FLASH on Bank 0 - Size = 0x%08lx = %ld MB\n",
-		     size_b0, size_b0 >> 20);
+		printf("## Unknown FLASH on Bank 0 - Size = 0x%08lx = %ld MB\n",
+			size_b0, size_b0 >> 20);
 	}
+
+	(void)flash_protect(FLAG_PROTECT_SET,CFG_FLASH0_BASE,(flash_info[0].start[6] - 1),&flash_info[0]);
+
 	return size_b0;
 }
 
@@ -77,8 +106,8 @@ void flash_print_info(flash_info_t * info)
 		if ((i % 5) == 0)
 			printf("\n   ");
 		printf(" %08lX%s",
-		       info->start[i],
-		       info->protect[i] ? " (RO)" : "     ");
+			info->start[i],
+			info->protect[i] ? " (RO)" : "     ");
 	}
 	printf("\n");
 	return;
@@ -93,11 +122,11 @@ int flash_erase(flash_info_t * info, int s_first, int s_last)
 
 	if (cnt == FLASH_TOT_SECT) {
 		printf(" Please Wait \n");
-		rc =  EraseFlash(); 
+		rc =  EraseFlash();
 		printf("done\n");
 	} else if (cnt > 0) {
 		printf("Sector erasing, Please Wait\n");
-		rc = FLASH_Block_Erase(s_first,s_last);
+		rc = FLASH_Block_Erase(info,s_first,s_last);
 		printf("done\n");
 	}
 	else {
@@ -197,7 +226,7 @@ int write_word(flash_info_t *info, ulong addr, ulong data)
 
 	/* Check if Flash is (sufficiently) erased */
 	if ((*dest & sdata) != sdata) {
-        	return ERR_NOT_ERASED;
+		return ERR_NOT_ERASED;
 	}
 
 	LED6_On();
@@ -207,7 +236,7 @@ int write_word(flash_info_t *info, ulong addr, ulong data)
 	FLASH_Base[WRITE_CMD3] = WRITE_DATA3;
 	if(flag)
 		enable_interrupts();
-    	*dest = sdata;
+	*dest = sdata;
 	asm("ssync;");
 	rcode = FlashDataToggle(CFG_FLASH_WRITE_TOUT);
 	if (rcode != ERR_OK) {
@@ -225,14 +254,14 @@ int ResetFlash()
 	FLASH_Base[ANY_OFF] = RESET_DATA;
 
 	/* Pause for 10 micro seconds */
-	udelay(10);	
+	udelay(10);
 
 	return ERR_OK;
 }
 
 int EraseFlash()
 {
-	FLASH_Base[WRITE_CMD1] = ERASE_DATA1; 
+	FLASH_Base[WRITE_CMD1] = ERASE_DATA1;
 	FLASH_Base[WRITE_CMD2] = ERASE_DATA2;
 	FLASH_Base[WRITE_CMD3] = ERASE_DATA3;
 	FLASH_Base[WRITE_CMD1] = ERASE_DATA4;
@@ -274,9 +303,9 @@ int FlashDataToggle(unsigned long timeout)
 			ResetFlash();
 			return ERR_PROG_ERROR;
 		}
-	}while(now < timeout);
+	} while(now < timeout);
 
-	printf("TimeOut - DataToggle\n");	
+	printf("TimeOut - DataToggle\n");
 	return ERR_TIMOUT;
 }
 
@@ -297,18 +326,29 @@ int GetCodes()
 	return ((man_id << 16) | dev_id);
 }
 
-int FLASH_Block_Erase(unsigned long s_first,unsigned long s_last)
+int FLASH_Block_Erase(flash_info_t *info, unsigned long s_first,unsigned long s_last)
 {
-	int rc;
+	int rc,prot,sect;
 	unsigned long Off;
-  	unsigned long start,now,timeout;
+	unsigned long start,now,timeout;
 
 	printf("Erasing Blocks %i to %i\n", s_first, s_last);
 
-        /* Timer critical section every block must be added in 50 us*/
-        /* Since we can not ensure that the 50us is met each time   */
-        /* due to not knowing the SCLK, or the wait states, We need */
-        /* to add one Block at a time                               */
+	/* Check for protected sectors */
+	prot = 0;
+	for (sect = s_first; sect <= s_last; ++sect) {
+		if (info->protect[sect])
+			prot++;
+	}
+	if (prot)
+		printf ("- Warning: %d protected sectors will not be erased!\n", prot);
+	else
+		printf ("\n");
+
+	/* Timer critical section every block must be added in 50 us*/
+	/* Since we can not ensure that the 50us is met each time   */
+	/* due to not knowing the SCLK, or the wait states, We need */
+	/* to add one Block at a time                               */
 	/* Tried adding more than one block at a time, but it does  */
  	/* not work in all cases, on all boards			    */
 
@@ -316,38 +356,37 @@ int FLASH_Block_Erase(unsigned long s_first,unsigned long s_last)
 	timeout = CFG_FLASH_ERASEBLOCK_TOUT * 2;
 
 	for (; s_first <= s_last; s_first++) {
-		putc ('.');
-		/* Determine offset */
-		Off = GetOffset(s_first);
-	
-		FLASH_Base[WRITE_CMD1] = ERASE_DATA1;
-		FLASH_Base[WRITE_CMD2] = ERASE_DATA2;
-		FLASH_Base[WRITE_CMD3] = ERASE_DATA3;
-		FLASH_Base[WRITE_CMD1] = ERASE_DATA4;
-		FLASH_Base[WRITE_CMD2] = ERASE_DATA5;
+		if (info->protect[s_first] == 0) {	/* not protected */
+			putc ('.');
+			/* Determine offset */
+			Off = GetOffset(s_first);
 
-		*(volatile unsigned short *) (Off) = 0x30;
-		asm("ssync;");
+			FLASH_Base[WRITE_CMD1] = ERASE_DATA1;
+			FLASH_Base[WRITE_CMD2] = ERASE_DATA2;
+			FLASH_Base[WRITE_CMD3] = ERASE_DATA3;
+			FLASH_Base[WRITE_CMD1] = ERASE_DATA4;
+			FLASH_Base[WRITE_CMD2] = ERASE_DATA5;
 
-		start = get_timer(0);
-		/* Wait for the Erase Timer Bit (DQ3) to be set */
-		while(1) {
-			now = get_timer(start);
-			if((FLASH_Base[0] & 0x0008) == 0x0008) 
-				break; /* Break when device starts the erase cycle */
-			if (now > timeout) {
-				printf("Time out - Erase cycle is not started\n");
-				return ERR_TIMOUT;
+			*(volatile unsigned short *) (Off) = 0x30;
+			asm("ssync;");
+
+			start = get_timer(0);
+			/* Wait for the Erase Timer Bit (DQ3) to be set */
+			while(1) {
+				now = get_timer(start);
+				if((FLASH_Base[0] & 0x0008) == 0x0008)
+					break; /* Break when device starts the erase cycle */
+				if (now > timeout) {
+					printf("Time out - Erase cycle is not started\n");
+					return ERR_TIMOUT;
+				}
+			}
+			/* Now wait for things to be done */
+			if ( (rc = FlashDataToggle(timeout)) != ERR_OK ) {
+				ResetFlash();
+				return rc;
 			}
 		}
-
-		/* Now wait for things to be done */
-		if ( (rc = FlashDataToggle(timeout)) != ERR_OK ) {
-	                ResetFlash();
-                	return rc;
-		}
-
-	} 
-
+	}
 	return ERR_OK;
 }
