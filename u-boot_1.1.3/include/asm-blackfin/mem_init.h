09445e83b526bde518a4d6511eb1d1c77d656149
Aubrey Li <aubrey.li@analog.com>
2005-08-31 08:20:48 +0000

    Add new SDRAM device configuration for BF537-STAMP board MT48LC8A2TG-75
    git-svn-id: svn://localhost/svn/u-boot/trunk@295 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.3/include/asm-blackfin/mem_init.h
+++ u-boot_1.1.3/include/asm-blackfin/mem_init.h
@@ -22,7 +22,7 @@
  * MA 02111-1307 USA
  */
 
-#if ( CONFIG_MEM_MT48LC16M16A2TG_75  ||  CONFIG_MEM_MT48LC64M4A2FB_7E )
+#if ( CONFIG_MEM_MT48LC16M16A2TG_75  ||  CONFIG_MEM_MT48LC64M4A2FB_7E || CONFIG_MEM_MT48LC16M8A2TG_75)
 	#if ( CONFIG_SCLK_HZ > 119402985 )
 		#define SDRAM_tRP	TRP_2
 		#define SDRAM_tRP_num	2
@@ -109,6 +109,13 @@
 	#define SDRAM_Tref	64       /* Refresh period in milliseconds   */
 	#define SDRAM_NRA	8192     /* Number of row addresses in SDRAM */
 	#define SDRAM_CL	CL_2
+#endif
+
+#if (CONFIG_MEM_MT48LC16M8A2TG_75)
+        /*SDRAM INFORMATION: */
+        #define SDRAM_Tref      64       /* Refresh period in milliseconds   */
+        #define SDRAM_NRA       4096     /* Number of row addresses in SDRAM */
+        #define SDRAM_CL        CL_3
 #endif
 
 #if ( CONFIG_MEM_SIZE == 128 )
