6da816bb89f63cf9c5aa2113694eae00c6a4978c
Mike Frysinger <vapier@gentoo.org>
2006-06-21 02:16:42 +0000

    cleanup the script, add proper exit status checking, and off load makefile tasks to the makefile
    git-svn-id: svn://localhost/svn/u-boot/trunk@417 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot_1.1.3/tools/bin2ldr/runme.sh
+++ u-boot_1.1.3/tools/bin2ldr/runme.sh
@@ -1,13 +1,16 @@
-# strongly recommend to run this script to convert u-boot.bin to LDR format file
 #!/bin/sh
-if [ -e ../../u-boot.bin ];then
-	make clean
-	make all
-	./bin2ldr
-	bfin-uclinux-objcopy -I binary -O ihex app.ldr app.hex
-	make distclean
-else
-	echo -e "ERR:Please build the u-boot package first"
+
+# strongly recommended to run this script to convert u-boot.bin to LDR format file
+
+if [ ! -e bin2ldr ] ; then
+	make all || exit 1
 fi
 
-
+if [ -e ../../u-boot.bin ] ; then
+	./bin2ldr || exit 1
+	bfin-uclinux-objcopy -I binary -O ihex app.ldr app.hex || exit 1
+	exit 0
+else
+	echo "ERR: Please build the u-boot package first"
+	exit 1
+fi
