98be0d3ec692ae2f8fff723555dcdfac485286f8
Aubrey Li <aubrey.li@analog.com>
2007-03-10 04:30:11 +0000

    [#2924] Add the setup mac address functionality back and coding style fixed
    git-svn-id: svn://localhost/svn/u-boot/trunk@657 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/board/bf537-stamp/bf537-stamp.c
+++ u-boot-1.1.6/board/bf537-stamp/bf537-stamp.c
@@ -40,9 +40,9 @@
  *   to also add booting from SPI, or TWI, but this function does
  *   not currently support that.
  */
-int do_bootldr (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
+int do_bootldr(cmd_tbl_t * cmdtp, int flag, int argc, char *argv[])
 {
-	ulong   addr, entry;
+	ulong addr, entry;
 	ulong *data;
 
 	/* Get the address */
@@ -53,43 +53,39 @@ int do_bootldr (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	}
 
 	/* Check if it is a LDR file */
-	data = (ulong *)addr;
-	if ( *data == 0xFF800060 || *data == 0xFF800040 || *data == 0xFF800020 ) {
+	data = (ulong *) addr;
+	if (*data == 0xFF800060 || *data == 0xFF800040 || *data == 0xFF800020) {
 		/* We want to boot from FLASH or SDRAM */
 		entry = _BOOTROM_BOOT_DXE_FLASH;
-		printf ("## Booting ldr image at 0x%08lx ...\n", addr);
-		if (icache_status ())
+		printf("## Booting ldr image at 0x%08lx ...\n", addr);
+		if (icache_status())
 			icache_disable();
-		if (dcache_status ())
-			dcache_disable ();
+		if (dcache_status())
+			dcache_disable();
 
-		__asm__(
-			"R7=%[a];\n"
-			"P0=%[b];\n"
-			"JUMP (P0);\n"
-			:
-			: [a] "d" (addr), [b] "a" (entry)
-			: "R7", "P0" );
+	      __asm__("R7=%[a];\n" "P0=%[b];\n" "JUMP (P0);\n":
+	      :[a] "d"(addr),[b] "a"(entry)
+	      :"R7", "P0");
 
 	} else {
-		printf ("## No ldr image at address 0x%08lx\n", addr);
+		printf("## No ldr image at address 0x%08lx\n", addr);
 	}
 
 	return 0;
 }
 
-U_BOOT_CMD(bootldr,2,0,do_bootldr,
-	"bootldr - boot ldr image from memory\n",
-	"[addr]\n         - boot ldr image stored in memory\n");
+U_BOOT_CMD(bootldr, 2, 0, do_bootldr,
+	   "bootldr - boot ldr image from memory\n",
+	   "[addr]\n         - boot ldr image stored in memory\n");
 
 int checkboard(void)
 {
-#if (BFIN_CPU == ADSP_BF534)	
-	printf("CPU:   ADSP BF534 Rev.: 0.%d\n", *pCHIPID >>28);
-#elif (BFIN_CPU == ADSP_BF536)	
-	printf("CPU:   ADSP BF536 Rev.: 0.%d\n", *pCHIPID >>28);
-#else	
-	printf("CPU:   ADSP BF537 Rev.: 0.%d\n", *pCHIPID >>28);
+#if (BFIN_CPU == ADSP_BF534)
+	printf("CPU:   ADSP BF534 Rev.: 0.%d\n", *pCHIPID >> 28);
+#elif (BFIN_CPU == ADSP_BF536)
+	printf("CPU:   ADSP BF536 Rev.: 0.%d\n", *pCHIPID >> 28);
+#else
+	printf("CPU:   ADSP BF537 Rev.: 0.%d\n", *pCHIPID >> 28);
 #endif
 	printf("Board: ADI BF537 stamp board\n");
 	printf("       Support: http://blackfin.uclinux.org/\n");
@@ -98,40 +94,40 @@ int checkboard(void)
 
 #if defined(CONFIG_BFIN_IDE)
 
-void cf_outb(unsigned char val, volatile unsigned char* addr)
+void cf_outb(unsigned char val, volatile unsigned char *addr)
 {
-        *(addr) = val;
+	*(addr) = val;
 	__builtin_bfin_ssync();
 }
 
 unsigned char cf_inb(volatile unsigned char *addr)
 {
-        volatile unsigned char c;
+	volatile unsigned char c;
 
-        c = *(addr);
+	c = *(addr);
 	__builtin_bfin_ssync();
 
-        return c;
+	return c;
 }
 
 void cf_insw(unsigned short *sect_buf, unsigned short *addr, int words)
 {
-        int i;
+	int i;
 
-        for (i = 0;i < words; i++)
-                *(sect_buf + i) = *(addr);
+	for (i = 0; i < words; i++)
+		*(sect_buf + i) = *(addr);
 	__builtin_bfin_ssync();
 }
 
 void cf_outsw(unsigned short *addr, unsigned short *sect_buf, int words)
 {
-        int i;
+	int i;
 
-        for (i = 0;i < words; i++)
-                *(addr) = *(sect_buf + i);
+	for (i = 0; i < words; i++)
+		*(addr) = *(sect_buf + i);
 	__builtin_bfin_ssync();
 }
-#endif /* CONFIG_BFIN_IDE */
+#endif				/* CONFIG_BFIN_IDE */
 
 long int initdram(int board_type)
 {
@@ -140,7 +136,7 @@ long int initdram(int board_type)
 	int brate;
 	char *tmp = getenv("baudrate");
 	brate = simple_strtoul(tmp, NULL, 16);
-	printf("Serial Port initialized with Baud rate = %x\n",brate);
+	printf("Serial Port initialized with Baud rate = %x\n", brate);
 	printf("SDRAM attributes:\n");
 	printf("tRCD %d SCLK Cycles,tRP %d SCLK Cycles,tRAS %d SCLK Cycles"
 	       "tWR %d SCLK Cycles,CAS Latency %d SCLK cycles \n",
@@ -159,30 +155,26 @@ long int initdram(int board_type)
 int misc_init_r(void)
 {
 	char nid[32];
-	unsigned char *pMACaddr = (unsigned char *) 0x203F0000;
-	u8  SrcAddr[6] = {0x02,0x80,0xAD,0x20,0x31,0xB8};
+	unsigned char *pMACaddr = (unsigned char *)0x203F0000;
+	u8 SrcAddr[6] = { 0x02, 0x80, 0xAD, 0x20, 0x31, 0xB8 };
 
 #if (CONFIG_COMMANDS & CFG_CMD_NET)
 	/* The 0xFF check here is to make sure we don't use the address
 	 * in flash if it's simply been erased (aka all 0xFF values) */
-	if (getenv("ethaddr") == NULL && 
-			is_valid_ether_addr(pMACaddr)) {
+	if (getenv("ethaddr") == NULL && is_valid_ether_addr(pMACaddr)) {
 		sprintf(nid, "%02x:%02x:%02x:%02x:%02x:%02x",
 			pMACaddr[0], pMACaddr[1],
-			pMACaddr[2], pMACaddr[3],
-			pMACaddr[4], pMACaddr[5]);
-	} else {
-		sprintf(nid, "%02x:%02x:%02x:%02x:%02x:%02x",
-			SrcAddr[0], SrcAddr[1],
-			SrcAddr[2], SrcAddr[3],
-			SrcAddr[4], SrcAddr[5]);
+			pMACaddr[2], pMACaddr[3], pMACaddr[4], pMACaddr[5]);
+		setenv("ethaddr", nid);
 	}
-	setenv("ethaddr", nid);
+	if (getenv("ethaddr")) {
+		SetupMacAddr(SrcAddr);
+	}
 #endif
 
 #if defined(CONFIG_BFIN_IDE)
 #if defined(CONFIG_BFIN_CF_IDE)
- /*Disable ATASEL when we're in Common Memory Mode*/
+	/*Disable ATASEL when we're in Common Memory Mode */
 	cf_outb(0, CONFIG_CF_ATASEL_DIS);
 	udelay(1000);
 #endif
@@ -203,7 +195,7 @@ int misc_init_r(void)
 /* Using sw10-PF5 as the hotkey */
 int post_hotkeys_pressed(void)
 {
-		return 0;
+	return 0;
 }
 #else
 /* Using sw10-PF5 as the hotkey */
@@ -212,31 +204,30 @@ int post_hotkeys_pressed(void)
 	int delay = 3;
 	int i;
 	unsigned short value;
-	
-	*pPORTF_FER   &= ~PF5;
+
+	*pPORTF_FER &= ~PF5;
 	*pPORTFIO_DIR &= ~PF5;
-	*pPORTFIO_INEN|=  PF5;
-	
-	printf("########Press SW10 to enter Memory POST########: %2d ",delay);	
-	while(delay--){
-		for(i=0;i<100;i++){
+	*pPORTFIO_INEN |= PF5;
+
+	printf("########Press SW10 to enter Memory POST########: %2d ", delay);
+	while (delay--) {
+		for (i = 0; i < 100; i++) {
 			value = *pPORTFIO & PF5;
-			if(value != 0){
+			if (value != 0) {
 				break;
-				}
-			udelay(10000);		
 			}
-		printf("\b\b\b%2d ",delay);
+			udelay(10000);
+		}
+		printf("\b\b\b%2d ", delay);
 	}
 	printf("\b\b\b 0");
 	printf("\n");
-	if(value == 0)
+	if (value == 0)
 		return 0;
-	else
-		{
+	else {
 		printf("Hotkey has been pressed, Enter POST . . . . . .\n");
 		return 1;
-		}
+	}
 }
 #endif
 #endif
@@ -244,15 +235,13 @@ int post_hotkeys_pressed(void)
 #if defined(CONFIG_POST) || defined(CONFIG_LOGBUFFER)
 void post_word_store(ulong a)
 {
-	volatile ulong *save_addr =
-		(volatile ulong *)POST_WORD_ADDR;
+	volatile ulong *save_addr = (volatile ulong *)POST_WORD_ADDR;
 	*save_addr = a;
 }
 
 ulong post_word_load(void)
 {
-	volatile ulong *save_addr = 
-		(volatile ulong *)POST_WORD_ADDR;
+	volatile ulong *save_addr = (volatile ulong *)POST_WORD_ADDR;
 	return *save_addr;
 }
 #endif
@@ -266,44 +255,50 @@ int uart_post_test(int flags)
 #define BLOCK_SIZE 0x10000
 #define VERIFY_ADDR 0x2000000
 extern int erase_block_flash(int);
-extern int write_data(long lStart, long lCount, uchar *pnData);
+extern int write_data(long lStart, long lCount, uchar * pnData);
 int flash_post_test(int flags)
 {
 	unsigned short *pbuf, *temp;
-	int offset,n,i;
+	int offset, n, i;
 	int value = 0;
 	int result = 0;
 	printf("\n");
 	pbuf = (unsigned short *)VERIFY_ADDR;
 	temp = pbuf;
-	for(n=FLASH_START_POST_BLOCK;n<FLASH_END_POST_BLOCK;n++){
-		offset = (n - 7)*BLOCK_SIZE;
+	for (n = FLASH_START_POST_BLOCK; n < FLASH_END_POST_BLOCK; n++) {
+		offset = (n - 7) * BLOCK_SIZE;
 		printf("--------Erase   block:%2d..", n);
 		erase_block_flash(n);
 		printf("OK\r");
 		printf("--------Program block:%2d...", n);
-		write_data(CFG_FLASH_BASE+offset, BLOCK_SIZE, pbuf);
+		write_data(CFG_FLASH_BASE + offset, BLOCK_SIZE, pbuf);
 		printf("OK\r");
 		printf("--------Verify  block:%2d...", n);
-		for(i=0;i< BLOCK_SIZE;i+=2){
-			if( *(unsigned short *)(CFG_FLASH_BASE + offset +i)!=*temp++){
+		for (i = 0; i < BLOCK_SIZE; i += 2) {
+			if (*(unsigned short *)(CFG_FLASH_BASE + offset + i) !=
+			    *temp++) {
 				value = 1;
 				result = 1;
 			}
 		}
-		if(value)
+		if (value)
 			printf("failed\n");
 		else
-			printf("OK		%3d%%\r",(int)((n+1-FLASH_START_POST_BLOCK)*100/(FLASH_END_POST_BLOCK - FLASH_START_POST_BLOCK)));
+			printf("OK		%3d%%\r",
+			       (int)(
+				     (n + 1 -
+				      FLASH_START_POST_BLOCK) *
+				     100 / (FLASH_END_POST_BLOCK -
+					    FLASH_START_POST_BLOCK)));
 
 		temp = pbuf;
 		value = 0;
 	}
-	printf("\n");		
-	if(result)
+	printf("\n");
+	if (result)
 		return -1;
 	else
-	        return 0;
+		return 0;
 }
 
 /****************************************************
@@ -313,33 +308,33 @@ int flash_post_test(int flags)
  ****************************************************/
 int led_post_test(int flags)
 {
-	*pPORTF_FER   	&= ~(PF6|PF7|PF8|PF9|PF10|PF11);
-        *pPORTFIO_DIR 	|= PF6|PF7|PF8|PF9|PF10|PF11;
-        *pPORTFIO_INEN	&= ~(PF6|PF7|PF8|PF9|PF10|PF11);	
-	*pPORTFIO 	&= ~(PF6|PF7|PF8|PF9|PF10|PF11);
+	*pPORTF_FER &= ~(PF6 | PF7 | PF8 | PF9 | PF10 | PF11);
+	*pPORTFIO_DIR |= PF6 | PF7 | PF8 | PF9 | PF10 | PF11;
+	*pPORTFIO_INEN &= ~(PF6 | PF7 | PF8 | PF9 | PF10 | PF11);
+	*pPORTFIO &= ~(PF6 | PF7 | PF8 | PF9 | PF10 | PF11);
 	udelay(1000000);
 	printf("LED1 on");
-	*pPORTFIO	|= PF6;
+	*pPORTFIO |= PF6;
 	udelay(1000000);
 	printf("\b\b\b\b\b\b\b");
 	printf("LED2 on");
-	*pPORTFIO	|= PF7;
+	*pPORTFIO |= PF7;
 	udelay(1000000);
 	printf("\b\b\b\b\b\b\b");
 	printf("LED3 on");
-        *pPORTFIO       |= PF8;
-        udelay(1000000);
+	*pPORTFIO |= PF8;
+	udelay(1000000);
 	printf("\b\b\b\b\b\b\b");
 	printf("LED4 on");
-        *pPORTFIO       |= PF9;
+	*pPORTFIO |= PF9;
 	udelay(1000000);
 	printf("\b\b\b\b\b\b\b");
 	printf("LED5 on");
-        *pPORTFIO       |= PF10;
-        udelay(1000000);
+	*pPORTFIO |= PF10;
+	udelay(1000000);
 	printf("\b\b\b\b\b\b\b");
 	printf("lED6 on");
-        *pPORTFIO       |= PF11;
+	*pPORTFIO |= PF11;
 	printf("\b\b\b\b\b\b\b ");
 	return 0;
 }
@@ -350,88 +345,88 @@ int led_post_test(int flags)
  ************************************************/
 int button_post_test(int flags)
 {
-	int i,delay = 5;
-	unsigned short value = 0;	
+	int i, delay = 5;
+	unsigned short value = 0;
 	int result = 0;
 
-        *pPORTF_FER   &= ~(PF5|PF4|PF3|PF2);
-        *pPORTFIO_DIR &= ~(PF5|PF4|PF3|PF2);
-        *pPORTFIO_INEN|=  (PF5|PF4|PF3|PF2);
+	*pPORTF_FER &= ~(PF5 | PF4 | PF3 | PF2);
+	*pPORTFIO_DIR &= ~(PF5 | PF4 | PF3 | PF2);
+	*pPORTFIO_INEN |= (PF5 | PF4 | PF3 | PF2);
 
-        printf("\n--------Press SW10: %2d ",delay);
-        while(delay--){
-                for(i=0;i<100;i++){
-                        value = *pPORTFIO & PF5;
-                        if(value != 0){
-                                break;
-                                }
-                        udelay(10000);
-                        }
-                printf("\b\b\b%2d ",delay);
-        }
-	if(value!=0)
+	printf("\n--------Press SW10: %2d ", delay);
+	while (delay--) {
+		for (i = 0; i < 100; i++) {
+			value = *pPORTFIO & PF5;
+			if (value != 0) {
+				break;
+			}
+			udelay(10000);
+		}
+		printf("\b\b\b%2d ", delay);
+	}
+	if (value != 0)
 		printf("\b\bOK");
-	else{
+	else {
 		result = -1;
-		printf("\b\bfailed");
-	}	
-
-	delay = 5;
-	printf("\n--------Press SW11: %2d ",delay);
-        while(delay--){
-                for(i=0;i<100;i++){
-                        value = *pPORTFIO & PF4;
-                        if(value != 0){
-                                break;
-                                }
-                        udelay(10000);
-                        }
-                printf("\b\b\b%2d ",delay);
-        }
-        if(value!=0)
-                printf("\b\bOK");
-        else{
-                result = -1;
 		printf("\b\bfailed");
 	}
 
 	delay = 5;
-        printf("\n--------Press SW12: %2d ",delay);
-        while(delay--){
-                for(i=0;i<100;i++){
-                        value = *pPORTFIO & PF3;
-                        if(value != 0){
-                                break;
-                                }
-                        udelay(10000);
-                        }
-                printf("\b\b\b%2d ",delay);
-        }
-        if(value!=0)
-                printf("\b\bOK");
-        else{
-                result = -1;
-                printf("\b\bfailed");
-        }
+	printf("\n--------Press SW11: %2d ", delay);
+	while (delay--) {
+		for (i = 0; i < 100; i++) {
+			value = *pPORTFIO & PF4;
+			if (value != 0) {
+				break;
+			}
+			udelay(10000);
+		}
+		printf("\b\b\b%2d ", delay);
+	}
+	if (value != 0)
+		printf("\b\bOK");
+	else {
+		result = -1;
+		printf("\b\bfailed");
+	}
 
 	delay = 5;
-        printf("\n--------Press SW13: %2d ",delay);
-        while(delay--){
-                for(i=0;i<100;i++){
-                        value = *pPORTFIO & PF2;
-                        if(value != 0){
-                                break;
-                                }
-                        udelay(10000);
-                        }
-                printf("\b\b\b%2d ",delay);
-        }
-        if(value!=0)
-                printf("\b\bOK");
-        else{
-                result = -1;
-                printf("\b\bfailed");
-        }
+	printf("\n--------Press SW12: %2d ", delay);
+	while (delay--) {
+		for (i = 0; i < 100; i++) {
+			value = *pPORTFIO & PF3;
+			if (value != 0) {
+				break;
+			}
+			udelay(10000);
+		}
+		printf("\b\b\b%2d ", delay);
+	}
+	if (value != 0)
+		printf("\b\bOK");
+	else {
+		result = -1;
+		printf("\b\bfailed");
+	}
+
+	delay = 5;
+	printf("\n--------Press SW13: %2d ", delay);
+	while (delay--) {
+		for (i = 0; i < 100; i++) {
+			value = *pPORTFIO & PF2;
+			if (value != 0) {
+				break;
+			}
+			udelay(10000);
+		}
+		printf("\b\b\b%2d ", delay);
+	}
+	if (value != 0)
+		printf("\b\bOK");
+	else {
+		result = -1;
+		printf("\b\bfailed");
+	}
 	printf("\n");
 	return result;
 }
