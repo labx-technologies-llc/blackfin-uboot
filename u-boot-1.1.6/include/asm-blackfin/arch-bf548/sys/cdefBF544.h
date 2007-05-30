4ed27e28b51c6bda0ff848ce21281051b03cb1f6
Jerry Zeng <jerry.zeng@analog.com>
2007-05-30 07:04:35 +0000

    [#3278] BF54x porting, add include/asm-blackfin/arch-bf548
    git-svn-id: svn://localhost/svn/u-boot/trunk@766 e8ad1c53-384c-47db-b5ce-66ae1f620dea

---
+++ u-boot-1.1.6/include/asm-blackfin/arch-bf548/sys/cdefBF544.h
@@ -0,0 +1,25 @@
+/*
+** Copyright (C) 2006 Analog Devices Inc., All Rights Reserved.
+**
+************************************************************************************
+**
+** This include file contains a list of macro "defines" to enable the programmer
+** to use symbolic names for the ADSP-BF544 peripherals.
+**  
+************************************************************************************
+** System MMR Register Map
+************************************************************************************/
+
+#ifndef _CDEF_BF544_H
+#define _CDEF_BF544_H
+
+/* include all Core registers and bit definitions */
+#include <defBF544.h>
+
+/* include core specific register pointer definitions */
+#include <cdef_LPBlackfin.h>
+
+/* include header that was auto-generated from the ArchDef XML for the ADSP-BF544 */
+#include <sys/XML_cdefBF544.h>
+
+#endif /* _CDEF_BF544_H */
---
+++ u-boot-1.1.6/include/asm-blackfin/arch-bf548/sys/cdefBF549.h
@@ -0,0 +1,25 @@
+/*
+** Copyright (C) 2006 Analog Devices Inc., All Rights Reserved.
+**
+************************************************************************************
+**
+** This include file contains a list of macro "defines" to enable the programmer
+** to use symbolic names for the ADSP-BF549 peripherals.
+**  
+************************************************************************************
+** System MMR Register Map
+************************************************************************************/
+
+#ifndef _CDEF_BF549_H
+#define _CDEF_BF549_H
+
+/* include all Core registers and bit definitions */
+//#include <defBF549.h>
+
+/* include core specific register pointer definitions */
+#include "sys/cdef_LPBlackfin.h"
+
+/* include header that was auto-generated from the ArchDef XML for the ADSP-BF549 */
+#include "sys/XML_cdefBF549.h"
+
+#endif /* _CDEF_BF549_H */
