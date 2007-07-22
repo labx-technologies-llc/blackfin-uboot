6d5154a3b2e6fb76a5ddb30fb66da302fc1b3c4e
Mike Frysinger <vapier@gentoo.org>
2007-07-22 07:44:03 +0000

    fix previous commit, only want cplbinfo, not bootldr
    git-svn-id: svn://localhost/svn/u-boot/trunk@859 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/include/configs/bf548-ezkit.h
+++ u-boot-1.1.6/include/configs/bf548-ezkit.h
@@ -132,8 +132,7 @@
 	 CFG_CMD_EEPROM)
 
 #define CONFIG_BFIN_COMMANDS \
-	( CFG_BFIN_CMD_BOOTLDR | \
-	  CFG_BFIN_CMD_CPLBINFO )
+	( CFG_BFIN_CMD_CPLBINFO )
 
 #define CONFIG_BOOTARGS "root=/dev/mtdblock0 rw"
 
