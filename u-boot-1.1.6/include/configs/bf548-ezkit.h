9bf282d1d0a7b7fd51ebfc556f56437e6dc1f5b9
Mike Frysinger <vapier@gentoo.org>
2007-10-01 07:01:44 +0000

    revert accidental BFIN_BOOT_MODE change in previous commit
    git-svn-id: svn://localhost/svn/u-boot/trunk@915 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/include/configs/bf548-ezkit.h
+++ u-boot-1.1.6/include/configs/bf548-ezkit.h
@@ -8,7 +8,7 @@
 #include <asm/blackfin-config-pre.h>
 
 #define BFIN_CPU             bf548-0.0
-#define BFIN_BOOT_MODE       BFIN_BOOT_UART
+#define BFIN_BOOT_MODE       BFIN_BOOT_PARA
 
 #define CFG_LONGHELP		1
 #define CONFIG_CMDLINE_EDITING	1
