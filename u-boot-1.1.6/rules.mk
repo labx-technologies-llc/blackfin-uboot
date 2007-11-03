8fd8148ee39ebab37dfe11fa339891c5ea629e03
Mike Frysinger <vapier@gentoo.org>
2007-11-03 21:07:15 +0000

    make .depend generation a little more parallel friendly
    git-svn-id: svn://localhost/svn/u-boot/trunk@1004 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/rules.mk
+++ u-boot-1.1.6/rules.mk
@@ -26,10 +26,6 @@
 _depend:	$(obj).depend
 
 $(obj).depend:	$(src)Makefile $(TOPDIR)/config.mk $(SRCS)
-		@rm -f $@
-		@for f in $(SRCS); do \
-			g=`basename $$f | sed -e 's/\(.*\)\.\w/\1.o/'`; \
-			$(CC) -M $(HOST_CFLAGS) $(CPPFLAGS) -MQ $(obj)$$g $$f >> $@ ; \
-		done
+		$(CC) -MM $(HOST_CFLAGS) $(CPPFLAGS) $(SRCS) | sed '/:/s|^|$(obj)|' > $@
 
 #########################################################################
