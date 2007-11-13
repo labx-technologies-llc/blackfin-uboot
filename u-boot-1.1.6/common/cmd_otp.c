163d98035568b1ccc3244148330133e314b27b03
Mike Frysinger <vapier@gentoo.org>
2007-11-13 02:08:46 +0000

    make people type "YES" when doing an otp write
    git-svn-id: svn://localhost/svn/u-boot/trunk@1023 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/common/cmd_otp.c
+++ u-boot-1.1.6/common/cmd_otp.c
@@ -38,26 +38,28 @@ static const char *otp_strerror(uint32_t err)
 	}
 }
 
+#define lowup(x) ((x) % 2 ? "upper" : "lower")
+
 int do_otp(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
-	if (argc < 4 || argc > 6) {
- usage:
-		printf("Usage:\n%s\n", cmdtp->usage);
-		return 1;
+	bool force = false;
+	if (!strcmp(argv[1], "--force")) {
+		force = true;
+		argv[1] = argv[0];
+		argv++;
+		--argc;
 	}
-
-	/* Only supported in newer silicon */
-#if (0)
-	otp_command(OTP_INIT, ...);
-#endif
 
 	uint32_t (*otp_func)(uint32_t page, uint32_t flags, uint64_t *page_content);
 	if (!strcmp(argv[1], "read"))
 		otp_func = otp_read;
 	else if (!strcmp(argv[1], "write"))
 		otp_func = otp_write;
-	else
-		goto usage;
+	else {
+ usage:
+		printf("Usage:\n%s\n", cmdtp->usage);
+		return 1;
+	}
 
 	uint64_t *addr = (uint64_t *)simple_strtoul(argv[2], NULL, 16);
 	uint32_t page = simple_strtoul(argv[3], NULL, 16);
@@ -79,6 +81,44 @@ int do_otp(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	} else
 		half = 0;
 
+	/* do to the nature of OTP, make sure users are sure */
+	if (!force && otp_func == otp_write) {
+		printf(
+			"Writing one time programmable memory\n"
+			"   source address:  0x%p\n"
+			"   OTP destination: %s page 0x%03X - %s page 0x%03X\n"
+			"   number to write: %ld halfpages\n"
+			" type \"YES\" (no quotes) to confirm: ",
+			addr,
+			lowup(half), page,
+			lowup(half + count - 1), page + (half + count - 1) / 2,
+			half + count
+		);
+
+		i = 0;
+		while (1) {
+			if (tstc()) {
+				const char exp_ans[] = "YES\r";
+				char c;
+				putc(c = getc());
+				if (exp_ans[i++] != c) {
+					printf(" Aborting\n");
+					return 1;
+				} else if (!exp_ans[i]) {
+					puts("\n");
+					break;
+				}
+			}
+		}
+
+		/* Only supported in newer silicon ... enable writing */
+#if (0)
+		otp_command(OTP_INIT, ...);
+#else
+		*pOTP_TIMING = 0x32149485;
+#endif
+	}
+
 	printf("OTP memory %s: addr 0x%08lx  page 0x%03X  count %ld ... ",
 		argv[1], addr, page, count);
 
@@ -98,9 +138,17 @@ int do_otp(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 	}
 	if (ret & 0x1)
 		printf("\nERROR at page 0x%03X (%s-halfpage): 0x%03X: %s\n",
-			page, (i % 2) ? "upper" : "lower", ret, otp_strerror(ret));
+			page, lowup(i), ret, otp_strerror(ret));
 	else
 		puts(" done\n");
+
+	if (otp_func == otp_write)
+		/* Only supported in newer silicon ... disable writing */
+#if (0)
+		otp_command(OTP_INIT, ...);
+#else
+		*pOTP_TIMING = 0x1485;
+#endif
 
 	return ret;
 }
@@ -108,7 +156,7 @@ int do_otp(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 U_BOOT_CMD(otp, 6, 0, do_otp,
 	"otp - One-Time-Programmable sub-system\n",
 	"read <addr> <page> [count] [half]\n"
-	"otp write <addr> <page> [count] [half]\n"
+	"otp write [--force] <addr> <page> [count] [half]\n"
 	"    - read/write 'count' half-pages starting at page 'page' (offset 'half')\n");
 
 #endif
