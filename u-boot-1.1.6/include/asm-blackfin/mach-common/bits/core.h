f0fa330423991f21753f8131cf38e834cbc12f77
Mike Frysinger <vapier@gentoo.org>
2007-12-21 09:25:31 +0000

    import EXCAUSE_P and HWERRCAUSE_P
    git-svn-id: svn://localhost/svn/u-boot/trunk@1068 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/include/asm-blackfin/mach-common/bits/core.h
+++ u-boot-1.1.6/include/asm-blackfin/mach-common/bits/core.h
@@ -47,6 +47,7 @@
  * SEQSTAT register
  */
 
+#define EXCAUSE_P	0	/* Last exception cause bit positions */
 #define EXCAUSE0_P	0	/* Last exception cause bit 0 */
 #define EXCAUSE1_P	1	/* Last exception cause bit 1 */
 #define EXCAUSE2_P	2	/* Last exception cause bit 2 */
@@ -55,6 +56,7 @@
 #define EXCAUSE5_P	5	/* Last exception cause bit 5 */
 #define IDLE_REQ_P	12	/* Pending idle mode request, set by IDLE instruction */
 #define SFTRESET_P	13	/* Indicates whether the last reset was a software reset (=1) */
+#define HWERRCAUSE_P	14	/* Last hw error cause bit positions */
 #define HWERRCAUSE0_P	14	/* Last hw error cause bit 0 */
 #define HWERRCAUSE1_P	15	/* Last hw error cause bit 1 */
 #define HWERRCAUSE2_P	16	/* Last hw error cause bit 2 */
