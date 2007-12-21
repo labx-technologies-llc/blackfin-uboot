a68be26b5cb5f164577a0aa9f1113ae9bf678d35
Mike Frysinger <vapier@gentoo.org>
2007-12-21 09:20:21 +0000

    split reset code off into its own file
    git-svn-id: svn://localhost/svn/u-boot/trunk@1066 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/cpu/blackfin/cpu.c
+++ u-boot-1.1.6/cpu/blackfin/cpu.c
@@ -35,59 +35,6 @@
 #include "cpu.h"
 #include "serial.h"
 
-/* A system soft reset makes external memory unusable so force
- * this function into L1.  We use the compiler ssync here rather
- * than SSYNC() because it's safe (no interrupts and such) and
- * we save some L1.  We do not need to force sanity in the SYSCR
- * register as the BMODE selection bit is cleared by the soft
- * reset while the Core B bit (on dual core parts) is cleared by
- * the core reset.
- */
-__attribute__ ((__l1_text__, __noreturn__))
-void bfin_reset(void)
-{
-	while (1) {
-		/* Not entirely sure why this ssync is needed ... */
-		__builtin_bfin_ssync();
-		/* Initiate system software reset of peripherals */
-		bfin_write_SWRST(0x7);
-		__builtin_bfin_ssync();
-		/* Clear system software reset */
-		bfin_write_SWRST(0);
-		__builtin_bfin_ssync();
-		/* Issue core reset */
-		asm("raise 1");
-	}
-}
-
-/* We need to trampoline ourselves up into L1 since our linker
- * does not have relaxtion support and will only generate a
- * PC relative call with a 25 bit immediate.  This is not enough
- * to get us from the top of SDRAM into L1.
- */
-__attribute__ ((__noreturn__))
-static inline void bfin_reset_trampoline(void)
-{
-	while (1)
-		asm("jump (%0);" : : "a" (bfin_reset));
-}
-
-__attribute__ ((__noreturn__))
-void bfin_reset_or_hang(void)
-{
-#ifdef CONFIG_PANIC_HANG
-	hang();
-#else
-	bfin_reset_trampoline();
-#endif
-}
-
-int do_reset(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
-{
-	bfin_reset_trampoline();
-	return 0;
-}
-
 void icache_enable(void)
 {
 	bfin_write_IMEM_CONTROL(bfin_read_IMEM_CONTROL() | (IMC | ENICPLB));
