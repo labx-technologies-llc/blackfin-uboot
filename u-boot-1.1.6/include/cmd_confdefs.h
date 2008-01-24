5aa10068a1d63bca4cd617317f09611afe49a6ed
Mike Frysinger <vapier@gentoo.org>
2008-01-24 17:37:38 +0000

    add support for a "strings" command
    git-svn-id: svn://localhost/svn/u-boot/trunk@1120 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/include/cmd_confdefs.h
+++ u-boot-1.1.6/include/cmd_confdefs.h
@@ -94,6 +94,7 @@
 #define CFG_CMD_EXT2	0x1000000000000000ULL	/* EXT2 Support			*/
 #define CFG_CMD_SNTP	0x2000000000000000ULL	/* SNTP support			*/
 #define CFG_CMD_DISPLAY	0x4000000000000000ULL	/* Display support		*/
+#define CFG_CMD_STRINGS	0x8000000000000000ULL	/* Strings support		*/
 
 #define CFG_CMD_ALL	0xFFFFFFFFFFFFFFFFULL	/* ALL commands			*/
 
