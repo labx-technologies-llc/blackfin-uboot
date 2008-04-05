ad653f79517d614324a58dfe5b543d994e4da835
Mike Frysinger <vapier@gentoo.org>
2008-04-05 04:15:54 +0000

    cleanup code a bit -- no functional changes
    git-svn-id: svn://localhost/svn/u-boot/trunk@1177 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/cpu/blackfin/i2c.c
+++ u-boot-1.1.6/cpu/blackfin/i2c.c
@@ -1,7 +1,7 @@
 /*
  * i2c.c - driver for Blackfin on-chip TWI/I2C
  *
- * Copyright (c) 2006-2007 Analog Devices Inc.
+ * Copyright (c) 2006-2008 Analog Devices Inc.
  *
  * Licensed under the GPL-2 or later.
  */
@@ -10,59 +10,35 @@
 
 #ifdef CONFIG_HARD_I2C
 
-#include <asm/blackfin.h>
 #include <i2c.h>
-#include <asm/io.h>
+
+#include <asm/blackfin.h>
 #include <asm/mach-common/bits/twi.h>
 
-/* Two-Wire Interface		(0xFFC01400 - 0xFFC014FF) */
+#define debugi(fmt, args...) debug("%s:%i: " fmt "\n", __func__, __LINE__, ## args)
+
 #ifdef TWI0_CLKDIV
-#define bfin_read_TWI_CLKDIV()               bfin_read_TWI0_CLKDIV()
 #define bfin_write_TWI_CLKDIV(val)           bfin_write_TWI0_CLKDIV(val)
 #define bfin_read_TWI_CONTROL()              bfin_read_TWI0_CONTROL()
 #define bfin_write_TWI_CONTROL(val)          bfin_write_TWI0_CONTROL(val)
-#define bfin_read_TWI_SLAVE_CTL()            bfin_read_TWI0_SLAVE_CTL()
-#define bfin_write_TWI_SLAVE_CTL(val)        bfin_write_TWI0_SLAVE_CTL(val)
-#define bfin_read_TWI_SLAVE_STAT()           bfin_read_TWI0_SLAVE_STAT()
-#define bfin_write_TWI_SLAVE_STAT(val)       bfin_write_TWI0_SLAVE_STAT(val)
-#define bfin_read_TWI_SLAVE_ADDR()           bfin_read_TWI0_SLAVE_ADDR()
-#define bfin_write_TWI_SLAVE_ADDR(val)       bfin_write_TWI0_SLAVE_ADDR(val)
-#define bfin_read_TWI_MASTER_CTL()           bfin_read_TWI0_MASTER_CTL()
-#define bfin_write_TWI_MASTER_CTL(val)       bfin_write_TWI0_MASTER_CTL(val)
-#define bfin_read_TWI_MASTER_STAT()          bfin_read_TWI0_MASTER_STAT()
-#define bfin_write_TWI_MASTER_STAT(val)      bfin_write_TWI0_MASTER_STAT(val)
-#define bfin_read_TWI_MASTER_ADDR()          bfin_read_TWI0_MASTER_ADDR()
-#define bfin_write_TWI_MASTER_ADDR(val)      bfin_write_TWI0_MASTER_ADDR(val)
 #define bfin_read_TWI_INT_STAT()             bfin_read_TWI0_INT_STAT()
 #define bfin_write_TWI_INT_STAT(val)         bfin_write_TWI0_INT_STAT(val)
-#define bfin_read_TWI_INT_MASK()             bfin_read_TWI0_INT_MASK()
-#define bfin_write_TWI_INT_MASK(val)         bfin_write_TWI0_INT_MASK(val)
-#define bfin_read_TWI_FIFO_CTL()             bfin_read_TWI0_FIFO_CTL()
-#define bfin_write_TWI_FIFO_CTL(val)         bfin_write_TWI0_FIFO_CTL(val)
-#define bfin_read_TWI_FIFO_STAT()            bfin_read_TWI0_FIFO_STAT()
-#define bfin_write_TWI_FIFO_STAT(val)        bfin_write_TWI0_FIFO_STAT(val)
-#define bfin_read_TWI_XMT_DATA8()            bfin_read_TWI0_XMT_DATA8()
+#define bfin_read_TWI_MASTER_STAT()          bfin_read_TWI0_MASTER_STAT()
+#define bfin_write_TWI_MASTER_STAT(val)      bfin_write_TWI0_MASTER_STAT(val)
 #define bfin_write_TWI_XMT_DATA8(val)        bfin_write_TWI0_XMT_DATA8(val)
-#define bfin_read_TWI_XMT_DATA_16()          bfin_read_TWI0_XMT_DATA16()
-#define bfin_write_TWI_XMT_DATA16(val)       bfin_write_TWI0_XMT_DATA16(val)
+#define bfin_read_TWI_MASTER_CTL()           bfin_read_TWI0_MASTER_CTL()
+#define bfin_write_TWI_MASTER_CTL(val)       bfin_write_TWI0_MASTER_CTL(val)
+#define bfin_write_TWI_MASTER_ADDR(val)      bfin_write_TWI0_MASTER_ADDR(val)
+#define bfin_write_TWI_INT_MASK(val)         bfin_write_TWI0_INT_MASK(val)
+#define bfin_write_TWI_FIFO_CTL(val)         bfin_write_TWI0_FIFO_CTL(val)
 #define bfin_read_TWI_RCV_DATA8()            bfin_read_TWI0_RCV_DATA8()
-#define bfin_write_TWI_RCV_DATA8(val)        bfin_write_TWI0_RCV_DATA8(val)
-#define bfin_read_TWI_RCV_DATA16()           bfin_read_TWI0_RCV_DATA16()
-#define bfin_write_TWI_RCV_DATA16(val)       bfin_write_TWI0_RCV_DATA16(val)
-#endif
-
-#ifdef DEBUG_I2C
-#define PRINTD(fmt,args...)	do {	\
-	DECLARE_GLOBAL_DATA_PTR;	\
-	if (gd->have_console)		\
-		printf(fmt ,##args);	\
-	} while (0)
-#else
-#define PRINTD(fmt,args...)
 #endif
 
 #ifndef CONFIG_TWICLK_KHZ
-#define CONFIG_TWICLK_KHZ	50
+# define CONFIG_TWICLK_KHZ	50
+#endif
+#if CONFIG_TWICLK_KHZ > 400
+# error The Blackfin I2C hardware can only operate at 400KHz max
 #endif
 
 /* All transfers are described by this data structure */
@@ -88,14 +64,10 @@ static void i2c_reset(void)
 	bfin_write_TWI_CONTROL(((get_sclk() / 1024 / 1024 + 5) / 10) & 0x7F);
 
 	/* Set Twi interface clock as specified */
-	if (CONFIG_TWICLK_KHZ > 400)
-		bfin_write_TWI_CLKDIV(((5 * 1024 / 400) << 8) | ((5 * 1024 /
-						400) & 0xFF));
-	else
-		bfin_write_TWI_CLKDIV(((5 * 1024 /
-					CONFIG_TWICLK_KHZ) << 8) | ((5 * 1024 /
-						CONFIG_TWICLK_KHZ)
-						& 0xFF));
+	bfin_write_TWI_CLKDIV(
+		((5 * 1024 / CONFIG_TWICLK_KHZ) << 8) |
+		((5 * 1024 / CONFIG_TWICLK_KHZ) & 0xFF)
+	);
 
 	/* Enable TWI */
 	bfin_write_TWI_CONTROL(bfin_read_TWI_CONTROL() | TWI_ENA);
@@ -213,7 +185,10 @@ int i2c_transfer(struct i2c_msg *msg)
 	}
 
 	while (bfin_read_TWI_MASTER_STAT() & BUSBUSY)
-		continue;
+		if (ctrlc()) {
+			ret = -5;
+			goto transfer_error;
+		}
 
 	/* Set Transmit device address */
 	bfin_write_TWI_MASTER_ADDR(msg->addr);
@@ -230,8 +205,7 @@ int i2c_transfer(struct i2c_msg *msg)
 	if (!(msg->flags & I2C_M_RD)) {
 		/* Transmit first data */
 		if (msg->len > 0) {
-			PRINTD("1 in i2c_transfer: buf=%d, len=%d\n", *msg->buf,
-			       len);
+			debugi("buf=%d, len=%d", *msg->buf, len);
 			bfin_write_TWI_XMT_DATA8(*(msg->buf++));
 			msg->len--;
 			SSYNC();
@@ -256,37 +230,25 @@ int i2c_transfer(struct i2c_msg *msg)
 
 	/* Master enable */
 	bfin_write_TWI_MASTER_CTL(bfin_read_TWI_MASTER_CTL() | MEN |
-			((msg->flags & I2C_M_RD)
-			 ? MDIR : 0) | ((CONFIG_TWICLK_KHZ >
-					 100) ? FAST : 0));
+			((msg->flags & I2C_M_RD) ? MDIR : 0) |
+			((CONFIG_TWICLK_KHZ > 100) ? FAST : 0)
+	);
 	SSYNC();
 
 	ret = wait_for_completion(msg, timeout_count);
-	PRINTD("3 in i2c_transfer: ret=%d\n", ret);
+	debugi("ret=%d", ret);
 
  transfer_error:
 	switch (ret) {
-	case 1:
-		PRINTD(("i2c_transfer: error: transfer fail\n"));
-		break;
-	case 2:
-		PRINTD(("i2c_transfer: error: transmit timeout\n"));
-		break;
-	case 3:
-		PRINTD(("i2c_transfer: error: ACK missing\n"));
-		break;
-	case 4:
-		PRINTD(("i2c_transfer: error: receive timeout\n"));
-		break;
-	case 5:
-		PRINTD(("i2c_transfer: error: controller not ready\n"));
+	case 1: debugi("error: transfer fail");    break;
+	case 2: debugi("error: transmit timeout"); break;
+	case 3: debugi("error: ACK missing");      break;
+	case 4: debugi("error: receive timeout");  break;
+	case 5: debugi("error: controller not ready");
 		i2c_reset();
-		break;
-	default:
 		break;
 	}
 	return ret;
-
 }
 
 /* ---------------------------------------------------------------------*/
@@ -344,18 +306,17 @@ int i2c_probe(uchar chip)
  *   Returns: 0 on success, not 0 on failure
  */
 
-int i2c_read(uchar chip, uint addr, int alen, uchar * buffer, int len)
+int i2c_read(uchar chip, uint addr, int alen, uchar *buffer, int len)
 {
 	struct i2c_msg msg;
 	u8 addr_bytes[3];	/* lowest...highest byte of data address */
 
-	PRINTD("i2c_read: chip=0x%x, addr=0x%x, alen=0x%x, len=0x%x\n", chip,
-			addr, alen, len);
+	debugi("chip=0x%x, addr=0x%x, alen=0x%x, len=0x%x", chip, addr, alen, len);
 
 	if (alen > 0) {
-		addr_bytes[0] = (u8) ((addr >> 0) & 0x000000FF);
-		addr_bytes[1] = (u8) ((addr >> 8) & 0x000000FF);
-		addr_bytes[2] = (u8) ((addr >> 16) & 0x000000FF);
+		addr_bytes[0] = (u8) ((addr >> 0) & 0xFF);
+		addr_bytes[1] = (u8) ((addr >> 8) & 0xFF);
+		addr_bytes[2] = (u8) ((addr >> 16) & 0xFF);
 		msg.addr = chip;
 		msg.flags = 0;
 		msg.len = alen;
@@ -365,7 +326,7 @@ int i2c_read(uchar chip, uint addr, int alen, uchar * buffer, int len)
 	}
 
 	/* start read sequence */
-	PRINTD(("i2c_read: start read sequence\n"));
+	debugi("start read sequence");
 	msg.addr = chip;
 	msg.flags = I2C_M_RD;
 	msg.len = len;
@@ -390,20 +351,19 @@ int i2c_read(uchar chip, uint addr, int alen, uchar * buffer, int len)
  *   Returns: 0 on success, not 0 on failure
  */
 
-int i2c_write(uchar chip, uint addr, int alen, uchar * buffer, int len)
+int i2c_write(uchar chip, uint addr, int alen, uchar *buffer, int len)
 {
 	struct i2c_msg msg;
 	u8 addr_bytes[3];	/* lowest...highest byte of data address */
 
-	PRINTD
-		("i2c_write: chip=0x%x, addr=0x%x, alen=0x%x, len=0x%x, buf0=0x%x\n",
-		 chip, addr, alen, len, buffer[0]);
+	debugi("chip=0x%x, addr=0x%x, alen=0x%x, len=0x%x, buf0=0x%x",
+		chip, addr, alen, len, buffer[0]);
 
 	/* chip address write */
 	if (alen > 0) {
-		addr_bytes[0] = (u8) ((addr >> 0) & 0x000000FF);
-		addr_bytes[1] = (u8) ((addr >> 8) & 0x000000FF);
-		addr_bytes[2] = (u8) ((addr >> 16) & 0x000000FF);
+		addr_bytes[0] = (u8) ((addr >> 0) & 0xFF);
+		addr_bytes[1] = (u8) ((addr >> 8) & 0xFF);
+		addr_bytes[2] = (u8) ((addr >> 16) & 0xFF);
 		msg.addr = chip;
 		msg.flags = 0;
 		msg.len = alen;
@@ -413,7 +373,7 @@ int i2c_write(uchar chip, uint addr, int alen, uchar * buffer, int len)
 	}
 
 	/* start read sequence */
-	PRINTD(("i2c_write: start write sequence\n"));
+	debugi("start write sequence");
 	msg.addr = chip;
 	msg.flags = 0;
 	msg.len = len;
@@ -422,22 +382,19 @@ int i2c_write(uchar chip, uint addr, int alen, uchar * buffer, int len)
 		return -1;
 
 	return 0;
-
 }
 
 uchar i2c_reg_read(uchar chip, uchar reg)
 {
 	uchar buf;
-
-	PRINTD("i2c_reg_read: chip=0x%02x, reg=0x%02x\n", chip, reg);
+	debugi("chip=0x%02x, reg=0x%02x", chip, reg);
 	i2c_read(chip, reg, 1, &buf, 1);
-	return (buf);
+	return buf;
 }
 
 void i2c_reg_write(uchar chip, uchar reg, uchar val)
 {
-	PRINTD("i2c_reg_write: chip=0x%02x, reg=0x%02x, val=0x%02x\n", chip,
-			reg, val);
+	debugi("chip=0x%02x, reg=0x%02x, val=0x%02x", chip, reg, val);
 	i2c_write(chip, reg, 1, &val, 1);
 }
 
