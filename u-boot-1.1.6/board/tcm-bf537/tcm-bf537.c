511b40b3ec31666e25a4199ffa76779fe990eec2
Mike Frysinger <vapier@gentoo.org>
2008-10-13 05:39:45 +0000

    set the Support URL for tiny boards to http://www.bluetechnix.at/
    git-svn-id: svn://localhost/svn/u-boot/trunk@1400 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/board/tcm-bf537/tcm-bf537.c
+++ u-boot-1.1.6/board/tcm-bf537/tcm-bf537.c
@@ -18,7 +18,7 @@ DECLARE_GLOBAL_DATA_PTR;
 int checkboard(void)
 {
 	printf("Board: Bluetechnix TCM-BF537 board\n");
-	printf("       Support: http://blackfin.uclinux.org/\n");
+	printf("       Support: http://www.bluetechnix.at/\n");
 	return 0;
 }
 
