00e11e83a18bc8e716c2a9cbaa0a703e5771b1e7
Mike Frysinger <vapier@gentoo.org>
2008-10-14 23:28:54 +0000

    flash on cm-bf561 supports hardware protection and powers up with it on by default
    git-svn-id: svn://localhost/svn/u-boot/trunk@1415 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-1.1.6/include/configs/cm-bf561.h
+++ u-boot-1.1.6/include/configs/cm-bf561.h
@@ -73,6 +73,7 @@
 #define CFG_FLASH_CFI		/* The flash is CFI compatible */
 #define CFG_FLASH_CFI_DRIVER	/* Use common CFI driver */
 #define CFG_FLASH_BASE		0x20000000
+#define CFG_FLASH_PROTECTION
 #define CFG_MAX_FLASH_BANKS	1	/* max number of memory banks */
 #define CFG_MAX_FLASH_SECT	67	/* max number of sectors on one chip; CM-BF561v2 has 8MB Flash */
 
