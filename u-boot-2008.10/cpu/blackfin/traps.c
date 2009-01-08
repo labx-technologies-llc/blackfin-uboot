bdbbc7503721aed05384a1468732ebf999c89b89
Mike Frysinger <vapier@gentoo.org>
2009-01-08 11:19:28 +0000

    revert part of previous patch to avoid const conversion warnings
    git-svn-id: svn://localhost/svn/u-boot/trunk@1591 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2008.10/cpu/blackfin/traps.c
+++ u-boot-2008.10/cpu/blackfin/traps.c
@@ -184,6 +184,7 @@ static const char *symbol_lookup(unsigned long addr, unsigned long *caddr)
 
 	extern const char system_map[] __attribute__((__weak__));
 	const char *sym, *csym;
+	char *esym;
 	unsigned long sym_addr;
 
 	sym = system_map;
@@ -191,7 +192,8 @@ static const char *symbol_lookup(unsigned long addr, unsigned long *caddr)
 	*caddr = 0;
 
 	while (*sym) {
-		sym_addr = simple_strtoul(sym, &sym, 16);
+		sym_addr = simple_strtoul(sym, &esym, 16);
+		sym = esym;
 		if (sym_addr > addr)
 			break;
 		*caddr = sym_addr;
