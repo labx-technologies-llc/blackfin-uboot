2dc378eda598c01258448674222b2bffa590224b
Mike Frysinger <vapier@gentoo.org>
2009-02-03 18:22:10 +0000

    backport cm-bf527 flash driver
    git-svn-id: svn://localhost/svn/u-boot/trunk@1640 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2008.10/include/configs/cm-bf527.h
+++ u-boot-2008.10/include/configs/cm-bf527.h
@@ -91,8 +91,8 @@
  */
 #define CFG_FLASH_BASE		0x20000000
 #define CFG_FLASH_CFI		/* The flash is CFI compatible */
-#define CONFIG_FLASH_CFI_DRIVER	/* Use common CFI driver */
-#define CFG_FLASH_PROTECTION
+#define FLASH_TOT_SECT		64
+#define CFG_FLASH_SIZE		0x800000
 #define CFG_MAX_FLASH_BANKS	1	/* max number of memory banks */
 #define CFG_MAX_FLASH_SECT	259	/* max number of sectors on one chip */
 
