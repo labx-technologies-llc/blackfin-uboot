60025e00d92da1e81957afe82ea0a06a79050290
Mike Frysinger <vapier@gentoo.org>
2009-02-22 00:47:51 +0000

    pull anomaly updates from toolchain
    git-svn-id: svn://localhost/svn/u-boot/trunk@1680 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2009.01/include/asm-blackfin/mach-bf527/anomaly.h
+++ u-boot-2009.01/include/asm-blackfin/mach-bf527/anomaly.h
@@ -2,7 +2,7 @@
  * File: include/asm-blackfin/mach-bf527/anomaly.h
  * Bugs: Enter bugs at http://blackfin.uclinux.org/
  *
- * Copyright (C) 2004-2008 Analog Devices Inc.
+ * Copyright (C) 2004-2009 Analog Devices Inc.
  * Licensed under the GPL-2 or later.
  */
 
@@ -167,7 +167,9 @@
 #define ANOMALY_05000263 (0)
 #define ANOMALY_05000266 (0)
 #define ANOMALY_05000273 (0)
+#define ANOMALY_05000278 (0)
 #define ANOMALY_05000285 (0)
+#define ANOMALY_05000305 (0)
 #define ANOMALY_05000307 (0)
 #define ANOMALY_05000311 (0)
 #define ANOMALY_05000312 (0)
