221e2458a116743c45fa4f229fe731f2888db0a5
Mike Frysinger <vapier@gentoo.org>
2009-03-23 09:05:03 +0000

    initial import of u-boot-2009.03
    git-svn-id: svn://localhost/svn/u-boot/trunk@1719 e8ad1c53-384c-47db-b5ce-66ae1f620dea

---
+++ u-boot-2009.03/drivers/usb/blackfin_usb.h
@@ -0,0 +1,27 @@
+/*
+ * Blackfin MUSB HCD (Host Controller Driver) for u-boot
+ *
+ * Copyright (c) 2008-2009 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2 or later.
+ */
+
+#ifndef __BLACKFIN_USB_H__
+#define __BLACKFIN_USB_H__
+
+#include "musb_core.h"
+
+/* Base address of Blackfin musb core */
+#define MENTOR_USB_BASE		(USB_FADDR)
+
+/* Blackfin uses DMA polling method of FIFO read/write */
+#define USB_DMA_BASE		USB_DMA_INTERRUPT
+#define USB_DMAx_CTRL		0x04
+#define USB_DMAx_ADDR_LOW	0x08
+#define USB_DMAx_ADDR_HIGH	0x0C
+#define USB_DMAx_COUNT_LOW	0x10
+#define USB_DMAx_COUNT_HIGH	0x14
+
+#define USB_DMA_REG(ep, reg)	(USB_DMA_BASE + 0x20 * ep + reg)
+
+#endif
