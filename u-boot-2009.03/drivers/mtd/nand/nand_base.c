83dcdf858d2be4c8bd47224c5dfd91dca7a7ef15
Graf Yang <graf.yang@analog.com>
2009-03-30 12:26:41 +0000

    Port u-boot commit [#1758] to 2009.03
    git-svn-id: svn://localhost/svn/u-boot/trunk@1760 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2009.03/drivers/mtd/nand/nand_base.c
+++ u-boot-2009.03/drivers/mtd/nand/nand_base.c
@@ -512,7 +512,7 @@ void nand_wait_ready(struct mtd_info *mtd)
 	reset_timer();
 
 	/* wait until command is processed or timeout occures */
-	while (get_timer(0) < timeo) {
+	while (get_timer(0) / 1000 < timeo) {
 		if (chip->dev_ready)
 			if (chip->dev_ready(mtd))
 				break;
@@ -862,7 +862,7 @@ static int nand_wait(struct mtd_info *mtd, struct nand_chip *this)
 	reset_timer();
 
 	while (1) {
-		if (get_timer(0) > timeo) {
+		if (get_timer(0) / 1000 > timeo) {
 			printf("Timeout!");
 			return 0x01;
 		}
@@ -877,7 +877,7 @@ static int nand_wait(struct mtd_info *mtd, struct nand_chip *this)
 	}
 #ifdef PPCHAMELON_NAND_TIMER_HACK
 	reset_timer();
-	while (get_timer(0) < 10);
+	while (get_timer(0) / 1000 < 10);
 #endif /*  PPCHAMELON_NAND_TIMER_HACK */
 
 	return this->read_byte(mtd);
