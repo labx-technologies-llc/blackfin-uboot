2ebc4642b32f7e0d3854c6f7dbafb5fafc671f35
Mike Frysinger <vapier@gentoo.org>
2009-04-24 21:38:20 +0000

    [#5035] convert blackfin-config-post.h to common config.h method
    git-svn-id: svn://localhost/svn/u-boot/trunk@1854 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2009.03/include/configs/bf537-pnav.h
+++ u-boot-2009.03/include/configs/bf537-pnav.h
@@ -5,7 +5,7 @@
 #ifndef __CONFIG_BF537_PNAV_H__
 #define __CONFIG_BF537_PNAV_H__
 
-#include <asm/blackfin-config-pre.h>
+#include <asm/config-pre.h>
 
 
 /*
@@ -159,7 +159,5 @@
  * Pull in common ADI header for remaining command/environment setup
  */
 #include <configs/bfin_adi_common.h>
-
-#include <asm/blackfin-config-post.h>
 
 #endif
--- u-boot-2009.03/include/configs/bf537-stamp.h
+++ u-boot-2009.03/include/configs/bf537-stamp.h
@@ -5,7 +5,7 @@
 #ifndef __CONFIG_BF537_STAMP_H__
 #define __CONFIG_BF537_STAMP_H__
 
-#include <asm/blackfin-config-pre.h>
+#include <asm/config-pre.h>
 
 
 /*
@@ -241,7 +241,5 @@
  * Pull in common ADI header for remaining command/environment setup
  */
 #include <configs/bfin_adi_common.h>
-
-#include <asm/blackfin-config-post.h>
 
 #endif
