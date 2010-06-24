1ad65261e3eb16ff9e7e1b53de78285545620a63
Michael Hennerich <michael.hennerich@analog.com>
2010-06-24 09:07:01 +0000

    [#6050] AD7160-Eval board support Enable CLKBUFOE since USB_XTALIN is connected to CLKBUF Disable RTC since there is no XTAL connected
    git-svn-id: svn://localhost/svn/u-boot/trunk@2335 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2010.03/board/bf527-ad7160-eval/bf527-ad7160-eval.c
+++ u-boot-2010.03/board/bf527-ad7160-eval/bf527-ad7160-eval.c
@@ -7,10 +7,19 @@
  */
 
 #include <common.h>
+#include <asm/blackfin.h>
+#include <asm/mach-common/bits/pll.h>
 
 int checkboard(void)
 {
 	printf("Board: ADI BF527 AD7160-EVAL board\n");
 	printf("       Support: http://blackfin.uclinux.org/\n");
+	return 0;
+}
+
+int misc_init_r(void)
+{
+	/* CLKIN Buffer Output Enable */
+	*pVR_CTL |= CLKBUFOE;
 	return 0;
 }
--- u-boot-2010.03/include/configs/bf527-ad7160-eval.h
+++ u-boot-2010.03/include/configs/bf527-ad7160-eval.h
@@ -136,7 +136,7 @@
 /*
  * Misc Settings
  */
-#define CONFIG_RTC_BFIN
+#define CONFIG_MISC_INIT_R
 #define CONFIG_UART_CONSOLE	0
 
 
