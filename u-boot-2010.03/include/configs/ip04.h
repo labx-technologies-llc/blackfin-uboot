01e97bc4120c0f54fc6b5ae130260b34ee3c0b1a
Mike Frysinger <vapier@gentoo.org>
2010-07-05 05:28:25 +0000

    ip04: drop duplicate NAND_PLAT_DEV_READY define that common code now handles
    git-svn-id: svn://localhost/svn/u-boot/trunk@2342 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2010.03/include/configs/ip04.h
+++ u-boot-2010.03/include/configs/ip04.h
@@ -124,7 +124,6 @@
 
 #define NAND_PLAT_WRITE_CMD(chip, cmd) BFIN_NAND_WRITE(BFIN_NAND_CLE(chip), cmd)
 #define NAND_PLAT_WRITE_ADR(chip, cmd) BFIN_NAND_WRITE(BFIN_NAND_ALE(chip), cmd)
-#define NAND_PLAT_DEV_READY(chip)      gpio_get_value(BFIN_NAND_READY)
 #define NAND_PLAT_GPIO_DEV_READY       GPIO_PF10
 
 
