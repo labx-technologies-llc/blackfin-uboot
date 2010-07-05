e4c1f03155dda1f7ca493883df53a13082d36c3a
Mike Frysinger <vapier@gentoo.org>
2010-07-05 06:29:19 +0000

    bfin: drop gcc-4.1 workaround now that we are dropping support for it in 2010R1
    git-svn-id: svn://localhost/svn/u-boot/trunk@2348 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- u-boot-2010.06/arch/blackfin/cpu/initcode.c
+++ u-boot-2010.06/arch/blackfin/cpu/initcode.c
@@ -92,17 +92,8 @@ static inline void serial_putc(char c)
 	if (!BFIN_DEBUG_EARLY_SERIAL)
 		return;
 
-	if (c == '\n') {
-#if (__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ <= 1)
-		/* gcc-4.1 sucks and cant inline this */
-		bfin_write16(&pUART->thr, '\r');
-
-		while (!(bfin_read16(&pUART->lsr) & TEMT))
-			continue;
-#else
+	if (c == '\n')
 		serial_putc('\r');
-#endif
-	}
 
 	bfin_write16(&pUART->thr, c);
 
