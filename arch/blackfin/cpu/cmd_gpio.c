ecd30c18f8adc90a5f2c234a3f20ddc343607ee9
Mike Frysinger <vapier@gentoo.org>
2010-08-02 18:08:04 +0000

    bfin: cmd_gpio: tail into common cmd_usage() func
    git-svn-id: svn://localhost/svn/u-boot/trunk@2372 e8ad1c53-384c-47db-b5ce-66ae1f620dea

--- arch/blackfin/cpu/cmd_gpio.c
+++ arch/blackfin/cpu/cmd_gpio.c
@@ -26,11 +26,9 @@ int do_gpio(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 		return 0;
 	}
 
-	if (argc != 3) {
+	if (argc != 3)
  show_usage:
-		cmd_usage(cmdtp);
-		return 1;
-	}
+		return cmd_usage(cmdtp);
 
 	/* parse the behavior */
 	ulong sub_cmd;
