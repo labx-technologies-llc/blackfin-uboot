d1aed4b03e1d119ac47406dcaa25a1646e15d273
Bob Liu <lliubbo@gmail.com>
2011-08-04 16:32:15 +0800

    devonshire: smc: fix pinmux
    Signed-off-by: Bob Liu <lliubbo@gmail.com>
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- board/bf609-ezkit/bf609-ezkit.c
+++ board/bf609-ezkit/bf609-ezkit.c
@@ -9,6 +9,7 @@
 #include <common.h>
 #include <netdev.h>
 #include <asm/blackfin.h>
+#include <asm/io.h>
 #include <asm/portmux.h>
 
 int checkboard(void)
@@ -23,7 +24,7 @@ int board_early_init_f(void)
 	static const unsigned short pins[] = {
 		P_A4, P_A5, P_A6, P_A7, P_A8, P_A9, P_A10, P_A11, P_A12, P_A13,
 		P_A14, P_A15, P_A16, P_A17, P_A18, P_A19, P_A20, P_A21, P_A22,
-		P_A23, P_A24, P_A25, P_AMS1, P_AMS2, P_AMS3, 0,
+		P_A23, P_A24, P_A25, P_AMS1, P_ABE0, P_ABE1, 0,
 	};
 	peripheral_request_list(pins, "smc");
 
@@ -38,13 +39,13 @@ int board_eth_init(bd_t *bis)
 	if (CONFIG_DW_PORTS & 1) {
 		static const unsigned short pins[] = P_RMII0;
 		if (!peripheral_request_list(pins, "emac0"))
-			ret += designware_initialize(0, EMAC0_MACCFG, 0);
+			ret += designware_initialize(0, EMAC0_MACCFG, 1);
 	}
 
 	if (CONFIG_DW_PORTS & 2) {
 		static const unsigned short pins[] = P_RMII1;
 		if (!peripheral_request_list(pins, "emac1"))
-			ret += designware_initialize(1, EMAC1_MACCFG, 0);
+			ret += designware_initialize(1, EMAC1_MACCFG, 1);
 	}
 
 	return ret;
