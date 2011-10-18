46b89db4fbffb42133598b024593d887f4da3802
Bob Liu <lliubbo@gmail.com>
2011-10-18 15:07:56 +0800

    blackfin: initcode: change coding style
    Signed-off-by: Bob Liu <lliubbo@gmail.com>
    Signed-off-by: Sonic Zhang <sonic.zhang@analog.com>

--- arch/blackfin/cpu/initcode.c
+++ arch/blackfin/cpu/initcode.c
@@ -412,7 +412,7 @@ program_clocks(ADI_BOOT_DATA *bs, bool put_into_srfs)
 
 	bfin_write_CGU_CTL(CONFIG_CGU_CTL_VAL);
 	bfin_write_CGU_DIV(CONFIG_CGU_DIV_VAL);
-	while (!((bfin_read_CGU_STAT() & 0xC) == 0x4))
+	while (!(bfin_read_CGU_STAT() & CLKSALGN))
 		continue;
 
 #else /* __ADSPBF60x__ */
